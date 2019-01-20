4f8352368b95904ce269533205b55dbee908ece8
Ritik <ritikgarg07@gmail.com>
2019-01-20 13:22:23 +0530

    added function to calculae poisiton

--- main.js
+++ main.js
@@ -1,56 +1,87 @@
 var mario = document.getElementById("mario");
-var block = document.getElementsByClassName("support onscreen");
+var block_unsorted = document.getElementsByClassName("support onscreen");
+
 //TODO NEXT: HEAD HITS ON BLOCK RATHER THAN PASSING THROUGH
+function getValue(element, property, units)
+{
+    if(units == 'vh')
+    {
+        return parseFloat(window.getComputedStyle(element).getPropertyValue(`${property}`))*100/window.innerHeight;   
+    }
+    else if(units == '%')
+    {
+        return parseFloat(window.getComputedStyle(element).getPropertyValue(`${property}`))*100/window.innerWidth;   
+    }
+}
+var block = Array.prototype.slice.call(block_unsorted).sort(function(a,b){
+    return getValue(b,"bottom","vh") - getValue(a,"bottom","vh");
+});
+
 
 function stay() {
     for(var ii = 0; ii < block.length; ii++)
     {
-        if(parseInt(window.getComputedStyle(mario).getPropertyValue("bottom")) > parseInt(window.getComputedStyle(block[ii]).getPropertyValue("bottom")) + parseInt(window.getComputedStyle(block[ii]).getPropertyValue("height")) &&  ((parseInt(window.getComputedStyle(mario).getPropertyValue("left")) + parseInt(window.getComputedStyle(mario).getPropertyValue("width")) >   parseInt(window.getComputedStyle(block[ii]).getPropertyValue("left")))   &&  parseInt(window.getComputedStyle(mario).getPropertyValue("left")) + parseInt(window.getComputedStyle(mario).getPropertyValue("width")) < parseInt(window.getComputedStyle(block[ii]).getPropertyValue("left")) + parseInt(window.getComputedStyle(block[ii]).getPropertyValue("width"))))
+        if(getValue(mario,"bottom","vh") > getValue(block[ii],"bottom","vh") + getValue(block[ii],"height","vh") &&  getValue(mario,"left","%") < getValue(block[ii],"left","%") + getValue(block[ii],"width","%") && getValue(mario,"left","%") + getValue(mario,"width","%") > getValue(block[ii],"left","%"))
         {   
             return ii;
         }
     } 
-
     return -1;
 }
-
-function move(e) {
-    console.log(parseInt(window.getComputedStyle(mario).getPropertyValue("left")) + parseInt(window.getComputedStyle(mario).getPropertyValue("width")));
-    console.log(stay());
+function moveUp(e) {
     e = e || window.event;
     if (e.keyCode == '38') {
         // up arrow
-        mario.style.bottom = parseInt(window.getComputedStyle(mario).getPropertyValue("bottom"))*100/window.innerHeight + 25 + "vh" ;
-            
+        mario.style.bottom = getValue(mario,"bottom","vh") + 25 + "vh" ;
     }
-    else if (e.keyCode == '40') {
-        // down arrow
-        if(stay() != -1)
+    setTimeout(function(){
+        if(stay() != -1) //supported
         {
-            mario.style.bottom = parseInt(window.getComputedStyle(block[stay()]).getPropertyValue("bottom"))*100/window.innerHeight + parseInt(window.getComputedStyle(block[stay()]).getPropertyValue("height"))*100/window.innerHeight + "vh";
+            mario.style.bottom = getValue(block[stay()],"bottom","vh") + getValue(block[stay()],"height","vh") + "vh";
         }
-    }
-    else if (e.keyCode == '37') {
-       // left arrow
-       for(var counter in block)
-       {
-            block[counter].style.left = parseInt(window.getComputedStyle(block[counter]).getPropertyValue("left"))*100/window.innerWidth + 5 + "%";
-       }
+        else mario.style.bottom = "20vh"
+    },285);
+}
+function moveDown(e)
+{
+    if (e.keyCode == '40') {
+            // down arrow
+        if(stay() != -1)
+            {
+                mario.style.down = 100 - getValue(block[stay()],"top","vh") + "vh";
+            }
+        }   
+}
+function moveSide(e)
+{
+
+    if (e.keyCode == '37') {
+           // left arrow
+       for(var ii = 0; ii < block.length; ii++)
+        {
+            block[ii].style.left = getValue(block[ii],"left","%") + 5 + "%";
+        }
     }
     else if (e.keyCode == '39') {
-       // right arrow
-       for(var counter in block)
-       {
-            block[counter].style.left = parseInt(window.getComputedStyle(block[counter]).getPropertyValue("left"))*100/window.innerWidth - 5 + "%";
-       }
-       
-    }
-    setTimeout(function(){if(stay() != -1) //supported
+        // right arrow
+       for(var ii = 0; ii < block.length; ii++)
         {
-            mario.style.bottom = parseInt(window.getComputedStyle(block[stay()]).getPropertyValue("bottom"))*100/window.innerHeight + parseInt(window.getComputedStyle(block[stay()]).getPropertyValue("height"))*100/window.innerHeight +  "vh";
+            block[ii].style.left = getValue(block[ii],"left","%") - 5 + "%";
         }
-        else mario.style.bottom = "20vh"},285);
-    
+    }
+    if(stay() != -1) {
+        mario.style.down = 100 - getValue(block[stay()],"top","vh") + "vh";
+    }
+    else mario.style.down = "20vh";            
 }
 
-    window.addEventListener("keydown",move);
+    window.addEventListener("keydown",moveUp);
+    window.addEventListener("keydown",moveDown);
+    window.addEventListener("keydown",moveSide);
+    if(stay() != -1) {
+        mario.style.down = 100 - getValue(block[stay()],"top","vh") + "vh";
+    }
+    else mario.style.down = "20vh";
+
+
+   
