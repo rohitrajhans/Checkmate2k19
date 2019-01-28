036e6cd40afd93b8171eb2b593377aff8d24bdcf
MuditShivendra <42600750+MuditShivendra@users.noreply.github.com>
2019-01-28 16:58:47 +0530

    text typing and modal animations updated

--- question pop up/question.js
+++ question pop up/question.js
@@ -1,10 +1,42 @@
-	var ques = document.getElementById('Ques');
-	var cross = document.getElementById('Notnot');
-		document.addEventListener('keydown', function abc(event) {
-				if (event.keyCode == 73 ) {//press i for question
-   					ques.className += ' questionBox';
-					}
+var ques = document.getElementById('Ques');
+var cross = document.getElementById('Notnot');
+var background = document.getElementById('bgrd');
+var i;
+
+
+
+document.addEventListener('keydown', function abc(event) {
+	if (event.keyCode == 73 ) {//press i for question
+		
+		bgrd.className += ' modalBackground';
+		ques.className += ' questionBox';
+		console.log(123);
+		function typeEffect(element, speed) {
+		var text = $(element).text();
+		$(element).html('');
+		i = 0;
+		var timer = setInterval(function() {
+						if (i < text.length) {
+							$(element).append(text.charAt(i));
+							i++;
+						} else {
+							clearInterval(timer);
+						}
+					}, speed);
+		}
+
+	$( document ).ready(function() {
+		var speed = 75;
+		var delay = $('h1').text().length * speed + speed;
+		typeEffect($('h1'), speed);
+		setTimeout(function(){
+		$('p').css('display', 'inline-block');
+		typeEffect($('p'), speed);
+		}, delay);
 		});
-		cross.addEventListener('click', function bcd(event) {//close the pop-up
-   					ques.className = 'hideBox';
-		});
+	}});
+
+	cross.addEventListener('click', function bcd(event) {//close the pop-up
+	ques.className = 'hideBox ';
+	bgrd.className = 'hideBox '; 
+	});
