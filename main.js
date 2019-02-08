59a0f8ea27189e4efb400d7506b61c1cd3d272e2
Ritik <ritikgarg07@gmail.com>
2019-02-08 05:53:34 +0530

    Added collisions back,(partially) jumps work

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 function getValue(element, property, units) { // property can only be x or y or height or width
-    // returns properties in fraction of vh/width
+    // returns properties in absolute values
     // origin is set to bottom left corner
     if (property == 'x') {
-        return parseFloat(element.getAttribute('transform').split('translate(').join(',').split(')').join(',').split(',')[1]) * 100 / 3360; // 3360 is viewport width of svg
+        return parseFloat(element.getAttribute('transform').split('translate(').join(',').split(')').join(',').split(',')[1]); // 3360 is viewport width of svg
     }
     if (property == 'y') {
-        return parseFloat(480 - element.getAttribute('transform').split('translate(').join(',').split(')').join(',').split(',')[2]) * 100 / 480; // 480 is viewport height if svg       
-    }
-    if (property == 'height') {
-        return parseFloat(element.getBoundingClientRect().height) * 100 / window.innerHeight;
-    }
-    if (property == 'width') {
-        return parseFloat(element.getBoundingClientRect().height) * 100 / window.innerWidth;
+        return parseFloat(element.getAttribute('transform').split('translate(').join(',').split(')').join(',').split(',')[2]); // 480 is viewport height if svg       
     }
 }
 
@@ -20,51 +14,50 @@ function getValueBlock(element, property) {
     if (block_group_num == 0) {
         return 456;
     } else {
-        if (property == 'x') {
-            // console.log('BlockGroup'  + block_group_num);
-            // console.log(document.getElementById('Blocks'));
-            return getValue(document.getElementById('Blocks'), 'x', '%') + getValue(document.getElementById('BlockGroup' + block_group_num), 'x', '%') + (parseFloat(element.getAttribute('x')) * 100 / 3360);
-        }
-        if (property == 'y') {
-            return getValue(document.getElementById('Blocks'), 'y', 'vh') + getValue(document.getElementById(`BlockGroup${block_group_num}`), 'y', 'vh') + ((480 - parseFloat(element.getAttribute('y'))) * 100 / 480);
-        }
         if (property == 'height') {
-            return parseFloat(element.getBoundingClientRect().height) * 100 / window.innerHeight;
+            return parseFloat(element.getAttribute('height')) * 100 / 480;
         }
         if (property == 'width') {
-            return parseFloat(element.getBoundingClientRect().height) * 100 / window.innerWidth;
+            return parseFloat(element.getAttribute('width')) * 100 / 3360;
+        }
+        if (property == 'x') {
+            return (getValue(document.getElementById('Blocks'), 'x', '%') + getValue(document.getElementById('BlockGroup' + block_group_num), 'x', '%') + parseFloat(element.getAttribute('x'))) * 100 / 3360;
+        }
+        if (property == 'y') {
+            return (480 - getValue(document.getElementById('Blocks'), 'y', 'vh') + getValue(document.getElementById(`BlockGroup${block_group_num}`), 'y', 'vh') + (parseFloat(element.getAttribute('y')))) * 100 / 480;
+        // be careful, the function is recursive and relies on the height statement being the y statement
         }
     }
 }
 
 function getValueGroup(element, property) {
     var group_num = parseInt(element.classList[3]);
-    if (property == 'x') {
-        return getValue(document.getElementById(`Group${group_num}`), 'x', '%') + (parseFloat(element.getAttribute('x')) * 100 / 3360);
-    }
-    if (property == 'y') {
-        return getValue(document.getElementById(`Group${group_num}`), 'y', 'vh') + ((480 - parseFloat(element.getAttribute('y')) * 100 / 480));
-    }
     if (property == 'height') {
-        return parseFloat(element.getBoundingClientRect().height) * 100 / window.innerHeight;
+        return parseFloat(element.getAttribute('height')) * 100 / 480;
     }
     if (property == 'width') {
-        return parseFloat(element.getBoundingClientRect().height) * 100 / window.innerWidth;
+        return parseFloat(element.getAttribute('width')) * 100 / 3360;
+    }
+    if (property == 'x') {
+        return (getValue(document.getElementById(`Group${group_num}`), 'x', '%') + parseFloat(element.getAttribute('x'))) * 100 / 3360;
+    }
+    if (property == 'y') {
+        return (480 - getValue(document.getElementById(`Group${group_num}`), 'y', 'vh')- parseFloat(element.getAttribute('y'))) * 100 / 480;
     }
 }
 
 function getValuePipes(element, property) {
-    if (property == 'x') {
-        return getValue(document.getElementById('Pipes'), 'x', '%') + (parseFloat(element.getAttribute('x')) * 100 / 3360);
-    }
-    if (property == 'y') {
-        return getValue(document.getElementById('Pipes'), 'y', 'vh') + ((480 - parseFloat(element.getAttribute('y')) * 100 / 480));
-    }
     if (property == 'height') {
-        return parseFloat(element.getBoundingClientRect().height) * 100 / window.innerHeight;
+        return parseFloat(element.getAttribute('height')) * 100 / 480;
     }
     if (property == 'width') {
-        return parseFloat(element.getBoundingClientRect().height) * 100 / window.innerWidth;
+        return parseFloat(element.getAttribute('width')) * 100 / 3360;
+    }
+    if (property == 'x') {
+        return (getValue(document.getElementById('Pipes'), 'x', '%') + parseFloat(element.getAttribute('x'))) * 100 / 3360;
+    }
+    if (property == 'y') {
+        return (480 - getValue(document.getElementById('Pipes'), 'y', 'vh') - parseFloat(element.getAttribute('y'))) * 100 / 480;
     }
 }
 var rest_left = [];
@@ -73,63 +66,63 @@ var rest_top = [];
 var blocks_svg = document.getElementsByClassName("blocks");
 for (var ii = 0; ii < blocks_svg.length; ++ii) {
     var temp = {
-        "left": getValueBlock(blocks_svg[ii], 'x'),
-        "bottom": getValueBlock(blocks_svg[ii], 'y'),
         "width": getValueBlock(blocks_svg[ii], 'width'),
-        "height": getValueBlock(blocks_svg[ii], 'height')
+        "height": getValueBlock(blocks_svg[ii], 'height'),
+        "left": getValueBlock(blocks_svg[ii], 'x'),
+        "top": getValueBlock(blocks_svg[ii], 'y'),
     };
     temp.right = temp.left + temp.width;
-    temp.top = temp.bottom + temp.height;
+    temp.bottom = temp.top - temp.height;
     rest_left.push(temp);
 }
 var groups_svg = document.getElementsByClassName("group");
 for (var ii = 0; ii < groups_svg.length; ++ii) {
     var temp = {
-        "left": getValueBlock(groups_svg[ii], 'x'),
-        "bottom": getValueBlock(groups_svg[ii], 'y'),
-        "width": getValueBlock(groups_svg[ii], 'width'),
-        "height": getValueBlock(groups_svg[ii], 'height')
+        "left": getValueGroup(groups_svg[ii], 'x'),
+        "top": getValueGroup(groups_svg[ii], 'y'),
+        "width": getValueGroup(groups_svg[ii], 'width'),
+        "height": getValueGroup(groups_svg[ii], 'height')
     };
     temp.right = temp.left + temp.width;
-    temp.top = temp.bottom + temp.height;
+    temp.bottom = temp.top - temp.height;
     rest_left.push(temp);
 }
 var pipes_svg = document.getElementsByClassName("group");
 for (var ii = 0; ii < pipes_svg.length; ++ii) {
     var temp = {
-        "left": getValueBlock(pipes_svg[ii], 'x'),
-        "bottom": getValueBlock(pipes_svg[ii], 'y'),
-        "width": getValueBlock(pipes_svg[ii], 'width'),
-        "height": getValueBlock(pipes_svg[ii], 'height')
+        "left": getValuePipes(pipes_svg[ii], 'x'),
+        "top": getValuePipes(pipes_svg[ii], 'y'),
+        "width": getValuePipes(pipes_svg[ii], 'width'),
+        "height": getValuePipes(pipes_svg[ii], 'height')
     };
     temp.right = temp.left + temp.width;
-    temp.top = temp.bottom + temp.height;
+    temp.bottom = temp.top - temp.height;
     rest_left.push(temp);
 }
-rest_left = rest_left.sort(function(a, b) {
-    return b.x - a.x;
+
+rest_left.sort(function(a, b) {
+    return a.left - b.left;
 }) // left to right
+
 for (var jj = 0; jj < rest_left.length; ++jj) {
     rest_top.push(rest_left[jj]);
 }
-rest_top = rest_top.sort(function(a, b) {
+rest_top.sort(function(a, b) {
     return b.top - a.top;
 }) // top to bottom
 
 var mario = {"element":document.getElementById("mario")};
-mario.left = getValue(mario.element, 'x','%');
-mario.bottom = getValue(mario.element, 'y',"vh");
-mario.width =  getValue(mario.element, 'width',"%");
-mario.height = getValue(mario.element, 'height',"vh");
+mario.width =  11*100/3360;
+mario.height = 15*100/480;
+mario.left = getValue(mario.element, 'x','%')*100/3360;
+mario.top = (480 - getValue(mario.element, 'y',"vh"))*100/480;
 mario.right = mario.left + mario.width;
-mario.top = mario.bottom + mario.height;
+mario.bottom = mario.top - mario.height;
 mario.defaultbottom = (480 - 433)*100/480;
 
 function updateMario(){
-    mario.left = getValue(mario.element, 'x','%');
-    mario.bottom = getValue(mario.element, 'y',"vh");
-    mario.width =  getValue(mario.element, 'width',"%");
-    mario.height = getValue(mario.element, 'height',"vh");
+    mario.left = getValue(mario.element, 'x','%')*100/3360;
+    mario.bottom = (480 - getValue(mario.element, 'y',"vh"))*100/480;
     mario.right = mario.left + mario.width;
     mario.top = mario.bottom + mario.height;
 }
@@ -150,7 +143,6 @@ function transformSvgElement(element, x, y) {
 }
 var flag_up = true;
 // var jump_dur = parseFloat(window.getComputedStyle(mario).getPropertyValue("transition-duration"))*1000;    //in ms
-var jump_dur = 200;         //ms
 
 function stay() {
     for (var ii = 0; ii < rest_top.length; ii++) {
@@ -161,12 +153,17 @@ function stay() {
     return -1;
 }
 
-var speed = 8;
+var ratio = 10000/3360;
+var jump_dur = 200;         //ms
+// var speed_rel = (speed/ratio)*100/3360;
+var speed_rel = 0.1;
+var speed = (speed_rel*3360/100)*ratio;              //pixels
 var pixelx = 0;
 var direction = 1; // not used rn
 var key_left = false;
 var key_right = false;
 var jumping = false;
+var jump_height = 15;     //percentage of 480
 
 function handleKeyDown(event) {
     if (event.keyCode == 37)
@@ -185,7 +182,7 @@ function handleKeyUp(event) {
 
 function onb() { //checks if on block and moves char down if reqd
     if (stay() != -1) {
-        transformSvgElement(mario.element,mario.left,rest_top[stay()].top);
+        transformSvgElement(mario.element,mario.left,rest_top[stay()].top + mario.height);
     } else {
         transformSvgElement(mario.element,mario.left,mario.defaultbottom);
     }
@@ -194,7 +191,6 @@ var falling;
 function moveUp(e) {
     e = e || window.event;
     if (e.keyCode == '38') {
-        moveSide();
         window.removeEventListener("keydown", moveUp);
         clearTimeout(falling);
         flag_up = false;
@@ -203,18 +199,18 @@ function moveUp(e) {
         jump_start: {
             for (var ii = rest_top.length - 1; ii > -1; --ii) //ascending order
             {
-                for (var temp = 0; temp < 26; temp++) // 25 is the normal jump height
-                {
-                    if (mario.left < rest_top[ii].right && mario.right > rest_top[ii].left && mario.top + temp > rest_top[ii].bottom && mario.bottom + temp < rest_top[ii].top) {
-                        transformSvgElement(mario.element,mario.left,rest_top[ii].bottom - mario.height);
+                if (mario.left < rest_top[ii].right && mario.right > rest_top[ii].left){
+                    if(rest_top[ii].bottom - mario.top <= jump_height && mario.top < rest_top[ii].bottom)
+                    {   
                         flag = 1;
+                        transformSvgElement(mario.element,mario.left,rest_top[ii].bottom - mario.height + mario.height);
                         break jump_start;
                     }
                 }
             }
         }
         if (flag == 0) {
-            transformSvgElement(mario.element,mario.left,mario.bottom + 25);
+            transformSvgElement(mario.element,mario.left,mario.bottom + jump_height);
         };
         setTimeout(function() {
             window.addEventListener("keydown", moveUp);
@@ -240,13 +236,13 @@ function moveDown(e) {
 
 
 function check_right() {
-    for (var ii = rest_left.length - 1; ii >= 0; --ii) {
-        var b_left = rest_left[ii].left - speed;
-
-        if (mario.right > b_left && mario.right < b_left + rest_left[ii].width) {
-            if ((mario.top < rest_left[ii].top && mario.top > rest_top[ii].top) ||
+    for (var ii = 0; ii < rest_left.length; ++ii) {
+        var b_left = rest_left[ii].left - speed_rel;
+        if (mario.right > b_left && mario.right < (b_left + rest_left[ii].width)) {
+            if ((mario.top < rest_left[ii].top && mario.top >= rest_left[ii].bottom) ||
                 (mario.top < rest_left[ii].bottom &&mario.top > rest_left[ii].top) ||
                 (mario.bottom < rest_left[ii].top && mario.bottom > rest_left.bottom)) {
+                console.log("stay right worked");
                 return ii;
             }
         }
@@ -256,42 +252,41 @@ function check_right() {
 
 function check_left() {
     for (var ii = rest_left.length - 1; ii >= 0; --ii) {
-        var b_right = rest_left[ii].right + speed;
+        var b_right = rest_left[ii].right + speed_rel;
         
-        if (mario.left <= b_right && mario.left >= b_right - rest_left[ii].width) {
-            if ((mario.top < rest_left[ii].top && mario.top > rest_left[ii].bottom) ||
-            (mario.bottom < rest_left[ii].bottom && mario.top > rest_left[ii].top) ||
+        if (mario.left < b_right && mario.left > (b_right - rest_left[ii].width)) {
+            if ((mario.top < rest_left[ii].top && mario.top >= rest_left[ii].bottom) ||
+            (mario.top < rest_left[ii].bottom && mario.top > rest_left[ii].top) ||
             (mario.bottom < rest_left[ii].top && mario.bottom > rest_left[ii].bottom))
             return ii;
         }
     }
     return -1;
 }
-var ratio = 10000/3360;
 function moveSide() {
     if (key_left == true) {
-        // x arrow
+        // left arrow
         if (check_left() == -1) {
-            pixelx += speed;
+            pixelx += speed_rel;
         } else pixelx += mario.left - rest_left[check_left()].right;
-        if(pixelx > speed){pixelx = speed;}
-        game.style.left = (parseFloat(window.getComputedStyle(document.getElementById("game")).getPropertyValue('left')) + pixelx) + 'px';         // in pixels here
-        transformSvgElement(mario.element,mario.left - (pixelx/ratio)*100/3360,mario.bottom);
+        if(pixelx > speed_rel){pixelx = speed_rel;}
+        game.style.left = (parseFloat(window.getComputedStyle(document.getElementById("game")).getPropertyValue('left'))) + ((pixelx*3360/100)*ratio) + 'px';         // in pixels here
+        transformSvgElement(mario.element,mario.left - pixelx,mario.bottom);
         pixelx = 0;
     }
 
     if (key_right == true) {
         // right arrow
         if (check_right() == -1) {
-            pixelx += speed;
-        } else pixelx += rest_left[check_right()].left - mario.right;
-        if(pixelx > speed){pixelx = speed;}
-        game.style.left = (parseFloat(window.getComputedStyle(document.getElementById("game")).getPropertyValue('left')) - pixelx) + 'px';             // in pixels here
-        transformSvgElement(mario.element,mario.left + (pixelx/ratio)*100/3360,mario.bottom);
+            pixelx += speed_rel;
+        } else pixelx += (rest_left[check_right()].left - mario.right);
+        if(pixelx > speed_rel){pixelx = speed_rel;}
+        game.style.left = (parseFloat(window.getComputedStyle(document.getElementById("game")).getPropertyValue('left')) - 
+        ((pixelx*3360/100)*ratio)) + 'px';             // in pixels here
+        transformSvgElement(mario.element,mario.left + pixelx,mario.bottom);
         pixelx = 0;
     }
 }
-
 window.addEventListener("keydown", moveUp);
 window.addEventListener("keydown", moveDown);
 window.addEventListener("keydown", handleKeyDown);
